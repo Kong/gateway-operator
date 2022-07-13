c505ed8aa627ddf8e00263f58e3e84b7423cf9c0
Mattia Lavacca <lavacca.mattia@gmail.com>
2022-07-13 15:21:23 +0200

    fix: namespace checked before being dereferenced
    Signed-off-by: Mattia Lavacca <lavacca.mattia@gmail.com>

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -30,3 +30,27 @@ func IsDataPlaneNotSet(err error) bool {
 	var onwRefErr *DataPlaneNotSetError
 	return errors.As(err, &onwRefErr)
 }
+
+// ObjectMissingParametersRefError is a custom object that must be raised when the
+// .spec.ParametersRef field of the given object is nil
+type ObjectMissingParametersRefError struct {
+	object metav1.Object
+
+	message string
+}
+
+func (err *ObjectMissingParametersRefError) Error() string {
+	return err.message
+}
+
+func NewObjectMissingParametersRef(obj metav1.Object) error {
+	return &ObjectMissingParametersRefError{
+		object:  obj,
+		message: fmt.Sprintf("object %s/%s has not reference to related objects", obj.GetNamespace(), obj.GetName()),
+	}
+}
+
+func IsObjectMissingParametersRef(err error) bool {
+	var onwRefErr *ObjectMissingParametersRefError
+	return errors.As(err, &onwRefErr)
+}
