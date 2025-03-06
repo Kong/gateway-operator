65ee4db59068bf718f6a40398f6054e579dea008
Jakub Warczarek <jakub.warczarek@konghq.com>
2025-03-06 17:43:13 +0100

    chore(tests): enable integration case `verifying DataPlane's NetworkPolicies...`

--- test/integration/test_gateway.go
+++ test/integration/test_gateway.go
@@ -770,9 +770,6 @@ func TestGatewayDataPlaneNetworkPolicy(t *testing.T) {
 	require.Contains(t, networkPolicy.Spec.Ingress, expectAllowMetricsIngress.Rule)
 
 	t.Run("verifying DataPlane's NetworkPolicies get updated after customizing kong proxy listen port through GatewayConfiguration", func(t *testing.T) {
-		// TODO: https://github.com/Kong/gateway-operator/issues/184
-		t.Skip("re-enable once https://github.com/Kong/gateway-operator/issues/184 is fixed")
-
 		gwcClient := GetClients().OperatorClient.GatewayOperatorV1beta1().GatewayConfigurations(namespace.Name)
 
 		setGatewayConfigurationEnvProxyPort(t, gatewayConfig, 8005, 8999)
