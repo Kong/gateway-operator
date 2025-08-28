Kong/kong-operator

feat: migrate KIC validation webhook

ingress-controller/internal/admission/server.go
@@ -24,13 +24,37 @@ var (
 )
 
 const (
-	DefaultAdmissionWebhookCertPath = "/admission-webhook/tls.crt"
-	DefaultAdmissionWebhookKeyPath  = "/admission-webhook/tls.key"
+	// https://github.com/kubernetes-sigs/controller-runtime/blob/3554729cfb3179c1a13f554b828d658d062dceb9/pkg/webhook/server.go#L81
+	DefaultAdmissionWebhookCertPath = "/tmp/k8s-webhook-server/serving-certs/tls.crt"
+	DefaultAdmissionWebhookKeyPath  = "/tmp/k8s-webhook-server/serving-certs/tls.key"
 )
 
 type Server struct {
 	s           *http.Server
 	certWatcher mo.Option[*certwatcher.CertWatcher]
+}
+
+func MakeTLSServer2(port int32, handler http.Handler) (*Server, error) {
+	const defaultHTTPReadHeaderTimeout = 10 * time.Second
+
+	watcher, err := certwatcher.New(DefaultAdmissionWebhookCertPath, DefaultAdmissionWebhookKeyPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create CertWatcher: %w", err)
+	}
+
+	return &Server{
+		s: &http.Server{
+			Addr: fmt.Sprintf(":%d", port),
+			TLSConfig: &tls.Config{
+				MinVersion:     tls.VersionTLS12,
+				MaxVersion:     tls.VersionTLS13,
+				GetCertificate: watcher.GetCertificate,
+			},
+			Handler:           handler,
+			ReadHeaderTimeout: defaultHTTPReadHeaderTimeout,
+		},
+		certWatcher: mo.Some(watcher),
+	}, nil
 }
 
 func MakeTLSServer(config managercfg.AdmissionServerConfig, handler http.Handler) (*Server, error) {
@@ -55,7 +79,7 @@ func MakeTLSServer(config managercfg.AdmissionServerConfig, handler http.Handler
 func (s *Server) Start(ctx context.Context) error {
 	logger := ctrllog.FromContext(ctx)
 	go func() {
-		if err := s.s.ListenAndServeTLS("", ""); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		if err := s.s.ListenAndServeTLS(DefaultAdmissionWebhookCertPath, DefaultAdmissionWebhookKeyPath); err != nil && !errors.Is(err, http.ErrServerClosed) {
 			logger.Error(err, "Failed to start admission server")
 		}
 	}()
@@ -75,6 +99,7 @@ func (s *Server) Start(ctx context.Context) error {
 	return s.s.Shutdown(ctx)
 }
 
+// TO REMOVE!!!
 func (s *Server) setupTLSConfig(sc managercfg.AdmissionServerConfig) (*tls.Config, error) {
 	var watcher *certwatcher.CertWatcher
 	var cert, key []byte

ingress-controller/validation2/webhook.go
@@ -1,0 +1,66 @@
+package validation2
+
+import (
+	"context"
+
+	ctrl "sigs.k8s.io/controller-runtime"
+
+	"github.com/kong/kong-operator/ingress-controller/internal/admission"
+	ctrlref "github.com/kong/kong-operator/ingress-controller/internal/controllers/reference"
+	"github.com/kong/kong-operator/ingress-controller/internal/dataplane/translator"
+	"github.com/kong/kong-operator/ingress-controller/internal/store"
+)
+
+// SetupAdmissionServer sets up the admission webhook server.
+func SetupAdmissionServer(
+	ctx context.Context,
+	m ctrl.Manager,
+	clientsManager admission.GatewayClientsProvider,
+	// referenceIndexers ctrlref.CacheIndexers,
+	// translatorFeatures translator.FeatureFlags,
+	storer store.Storer,
+) error {
+	admissionLogger := ctrl.LoggerFrom(ctx).WithName("admission-server")
+
+	adminAPIServicesProvider := admission.NewDefaultAdminAPIServicesProvider(clientsManager)
+
+	const ingressClassName = "kong"
+
+	// On the level of particular KIC instance for now enable all.
+	allEnabled := translator.FeatureFlags{
+		RewriteURIs:                             true,
+		FillIDs:                                 true,
+		ReportConfiguredKubernetesObjects:       true,
+		ExpressionRoutes:                        true,
+		KongServiceFacade:                       true,
+		EnterpriseEdition:                       true,
+		KongCustomEntity:                        true,
+		CombinedServicesFromDifferentHTTPRoutes: true,
+		SupportRedirectPlugin:                   true,
+	}
+
+	srv, err := admission.MakeTLSServer2(5443, &admission.RequestHandler{
+		Validator: admission.NewKongHTTPValidator(
+			admissionLogger,
+			m.GetClient(),
+			ingressClassName,
+			adminAPIServicesProvider,
+			allEnabled,
+			storer,
+		),
+		ReferenceIndexers: ctrlref.CacheIndexers{},
+		Logger:            admissionLogger,
+	})
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		admissionLogger.Info(">>> Starting admission server")
+		if err := srv.Start(ctx); err != nil {
+			admissionLogger.Error(err, "Admission server exited")
+		}
+	}()
+
+	return nil
+}

Kong/gateway-operator

ingress-controller/internal/admission/server.go
@@ -8,14 +8,12 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/samber/mo"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
 	"sigs.k8s.io/controller-runtime/pkg/certwatcher"
 	ctrllog "sigs.k8s.io/controller-runtime/pkg/log"
 
 	"github.com/kong/kong-operator/ingress-controller/internal/manager/consts"
-	managercfg "github.com/kong/kong-operator/ingress-controller/pkg/manager/config"
 )
 
 var (
@@ -24,102 +22,57 @@ var (
 )
 
 const (
-	DefaultAdmissionWebhookCertPath = "/admission-webhook/tls.crt"
-	DefaultAdmissionWebhookKeyPath  = "/admission-webhook/tls.key"
+	// https://github.com/kubernetes-sigs/controller-runtime/blob/3554729cfb3179c1a13f554b828d658d062dceb9/pkg/webhook/server.go#L81
+	defaultAdmissionWebhookCertPath = "/tmp/k8s-webhook-server/serving-certs/tls.crt"
+	defaultAdmissionWebhookKeyPath  = "/tmp/k8s-webhook-server/serving-certs/tls.key"
 )
 
 type Server struct {
 	s           *http.Server
-	certWatcher mo.Option[*certwatcher.CertWatcher]
+	certWatcher *certwatcher.CertWatcher
 }
 
-func MakeTLSServer(config managercfg.AdmissionServerConfig, handler http.Handler) (*Server, error) {
+func MakeTLSServer(port int32, handler http.Handler) (*Server, error) {
 	const defaultHTTPReadHeaderTimeout = 10 * time.Second
 
-	s := &Server{}
-	tlsConfig, err := s.setupTLSConfig(config)
+	watcher, err := certwatcher.New(defaultAdmissionWebhookCertPath, defaultAdmissionWebhookKeyPath)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to create CertWatcher: %w", err)
 	}
 
-	s.s = &http.Server{
-		Addr:              config.ListenAddr,
-		TLSConfig:         tlsConfig,
-		Handler:           handler,
-		ReadHeaderTimeout: defaultHTTPReadHeaderTimeout,
-	}
-	return s, nil
+	return &Server{
+		s: &http.Server{
+			Addr: fmt.Sprintf(":%d", port),
+			TLSConfig: &tls.Config{
+				MinVersion:     tls.VersionTLS12,
+				MaxVersion:     tls.VersionTLS13,
+				GetCertificate: watcher.GetCertificate,
+			},
+			Handler:           handler,
+			ReadHeaderTimeout: defaultHTTPReadHeaderTimeout,
+		},
+		certWatcher: watcher,
+	}, nil
 }
 
 // Start starts the admission server and blocks until the context is done.
 func (s *Server) Start(ctx context.Context) error {
 	logger := ctrllog.FromContext(ctx)
 	go func() {
-		if err := s.s.ListenAndServeTLS("", ""); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		if err := s.s.ListenAndServeTLS(defaultAdmissionWebhookCertPath, defaultAdmissionWebhookKeyPath); err != nil && !errors.Is(err, http.ErrServerClosed) {
 			logger.Error(err, "Failed to start admission server")
 		}
 	}()
 
-	if cw, ok := s.certWatcher.Get(); ok {
-		go func() {
-			if err := cw.Start(ctx); err != nil {
-				logger.Error(err, "Failed to start CertWatcher")
-			}
-		}()
-	}
+	go func() {
+		if err := s.certWatcher.Start(ctx); err != nil {
+			logger.Error(err, "Failed to start CertWatcher")
+		}
+	}()
 
 	<-ctx.Done()
 
 	ctx, cancel := context.WithTimeout(context.Background(), consts.DefaultGracefulShutdownTimeout) //nolint:contextcheck
 	defer cancel()
 	return s.s.Shutdown(ctx)
-}
-
-func (s *Server) setupTLSConfig(sc managercfg.AdmissionServerConfig) (*tls.Config, error) {
-	var watcher *certwatcher.CertWatcher
-	var cert, key []byte
-	switch {
-	// the caller provided certificates via the ENV (certwatcher can't be used here)
-	case sc.CertPath == "" && sc.KeyPath == "" && sc.Cert != "" && sc.Key != "":
-		cert, key = []byte(sc.Cert), []byte(sc.Key)
-		keyPair, err := tls.X509KeyPair(cert, key)
-		if err != nil {
-			return nil, fmt.Errorf("X509KeyPair error: %w", err)
-		}
-		return &tls.Config{
-			MinVersion:   tls.VersionTLS12,
-			MaxVersion:   tls.VersionTLS13,
-			Certificates: []tls.Certificate{keyPair},
-		}, nil
-
-	// the caller provided explicit file paths to the certs, enable certwatcher for these paths
-	case sc.CertPath != "" && sc.KeyPath != "" && sc.Cert == "" && sc.Key == "":
-		var err error
-		watcher, err = certwatcher.New(sc.CertPath, sc.KeyPath)
-		if err != nil {
-			return nil, fmt.Errorf("failed to create CertWatcher: %w", err)
-		}
-
-	// the caller provided no certificate configuration, assume the default paths and enable certwatcher for them
-	case sc.CertPath == "" && sc.KeyPath == "" && sc.Cert == "" && sc.Key == "":
-		var err error
-		watcher, err = certwatcher.New(DefaultAdmissionWebhookCertPath, DefaultAdmissionWebhookKeyPath)
-		if err != nil {
-			return nil, fmt.Errorf("failed to create CertWatcher: %w", err)
-		}
-
-	default:
-		return nil, fmt.Errorf("either cert/key files OR cert/key values must be provided, or none")
-	}
-
-	// If we have a watcher, we need to keep it to run it later in Start() method.
-	if watcher != nil {
-		s.certWatcher = mo.Some(watcher)
-	}
-
-	return &tls.Config{
-		MinVersion:     tls.VersionTLS12,
-		MaxVersion:     tls.VersionTLS13,
-		GetCertificate: watcher.GetCertificate,
-	}, nil
 }

ingress-controller/validation/webhook.go
@@ -1,0 +1,68 @@
+package validation
+
+import (
+	"context"
+
+	ctrl "sigs.k8s.io/controller-runtime"
+
+	"github.com/kong/kong-operator/ingress-controller/internal/admission"
+	ctrlref "github.com/kong/kong-operator/ingress-controller/internal/controllers/reference"
+	"github.com/kong/kong-operator/ingress-controller/internal/dataplane/translator"
+	"github.com/kong/kong-operator/ingress-controller/internal/store"
+)
+
+// SetupAdmissionServer sets up the admission webhook server.
+func SetupAdmissionServer(
+	ctx context.Context,
+	m ctrl.Manager,
+	// clientsManager admission.GatewayClientsProvider,
+	// referenceIndexers ctrlref.CacheIndexers,
+	// translatorFeatures translator.FeatureFlags,
+	// storer store.Storer,
+) error {
+	admissionLogger := ctrl.LoggerFrom(ctx).WithName("admission-server")
+
+	var clientsManager admission.GatewayClientsProvider
+	adminAPIServicesProvider := admission.NewDefaultAdminAPIServicesProvider(clientsManager)
+
+	const ingressClassName = "kong"
+
+	// On the level of particular KIC instance for now enable all.
+	allEnabled := translator.FeatureFlags{
+		RewriteURIs:                             true,
+		FillIDs:                                 true,
+		ReportConfiguredKubernetesObjects:       true,
+		ExpressionRoutes:                        true,
+		KongServiceFacade:                       true,
+		EnterpriseEdition:                       true,
+		KongCustomEntity:                        true,
+		CombinedServicesFromDifferentHTTPRoutes: true,
+		SupportRedirectPlugin:                   true,
+	}
+
+	var storer store.Storer
+	srv, err := admission.MakeTLSServer(5443, &admission.RequestHandler{
+		Validator: admission.NewKongHTTPValidator(
+			admissionLogger,
+			m.GetClient(),
+			ingressClassName,
+			adminAPIServicesProvider,
+			allEnabled,
+			storer,
+		),
+		ReferenceIndexers: ctrlref.CacheIndexers{},
+		Logger:            admissionLogger,
+	})
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		admissionLogger.Info(">>> Starting admission server")
+		if err := srv.Start(ctx); err != nil {
+			admissionLogger.Error(err, "Admission server exited")
+		}
+	}()
+
+	return nil
+}